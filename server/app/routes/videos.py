258b8d9047409d2fca578abdec4876d67cfc5add
abdmoh123 <abdhawisa@gmail.com>
2025-11-28 01:43:09 +0000

    feat(api): add video file uploading
    Added aiofiles package for asynchronous files writing (videos can be
    long to upload).
    
    Also renamed the create_video method to upload_video.
    
    Video can only be uploaded through the creation endpoint. Updating the
    video can only update the metadata stored in the database for now.

--- server/app/routes/videos.py
+++ server/app/routes/videos.py
@@ -1,6 +1,9 @@
 """FastAPI routes related to the Video table."""
 
-from fastapi import APIRouter, Depends, HTTPException
+from pathlib import Path
+
+import aiofiles
+from fastapi import APIRouter, Depends, HTTPException, UploadFile
 from sqlalchemy.orm import Session
 
 from app.api_schemas import VideoCreate, VideoUpdate, Video
@@ -11,8 +14,9 @@ from app.db_models import Video as VideoSchema
 
 router = APIRouter(prefix="/videos", tags=["videos"])
 
-
-# TODO: Add functionality for managing the video files (e.g. deleting an entry should also delete the actual file)
+# Make sure the path for storing the actual videos exists
+VIDEO_FILES_DIR = Path("/var/lib/pi-security-camera/videos")
+VIDEO_FILES_DIR.mkdir(parents=True, exist_ok=True)
 
 
 @router.get("/", response_model=list[Video])
@@ -22,15 +26,29 @@ def get_videos(page_index: int = 0, page_size: int = 100, db_session: Session = 
 
 
 @router.post("/", response_model=Video)
-def create_video(video: VideoCreate, db_session: Session = Depends(get_db)) -> VideoSchema:  # pyright: ignore[reportCallInDefaultInitializer]
-    """Creates a new video with the given details."""
-    db_videos: list[VideoSchema] = crud_video.get_video_entries_by_file_name(db_session, video.file_name)
+async def upload_video(
+    video_data: VideoCreate,
+    video_file: UploadFile,
+    db_session: Session = Depends(get_db),  # pyright: ignore[reportCallInDefaultInitializer]
+) -> VideoSchema:
+    """Creates and uploads a new video with the given details."""
+    db_videos: list[VideoSchema] = crud_video.get_video_entries_by_file_name(db_session, video_data.file_name)
 
+    # Check if the video entry already exists in the database
     for found_video in db_videos:
-        if found_video.camera_id == video.camera_id:
+        if found_video.camera_id == video_data.camera_id:
             raise HTTPException(status_code=400, detail="Video already exists!")
 
-    return crud_video.create_video_entry(db_session, video)
+    if video_file.content_type is None or "video" not in video_file.content_type:
+        raise HTTPException(status_code=404, detail="File uploaded is not a video!")
+
+    # Write the uploaded video to the server's storage (async part)
+    file_path: Path = VIDEO_FILES_DIR / str(video_data.camera_id) / video_data.file_name
+    video_contents: bytes = await video_file.read()
+    async with aiofiles.open(file_path, "wb") as file:
+        _ = await file.write(video_contents)
+
+    return crud_video.create_video_entry(db_session, video_data)
 
 
 @router.get("/{video_id}", response_model=Video)
