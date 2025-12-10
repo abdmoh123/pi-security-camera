4bb2073a5266a57f83aa75b5bedb021e1567c392
abdmoh123 <abdhawisa@gmail.com>
2025-12-10 23:24:50 +0000

    feat(validation): create re-usable class for pagination
    Validation consists of ensuring positive numbers.

---
+++ server/app/api/models/general.py
@@ -0,0 +1,10 @@
+"""This module contains pydantic models for general use cases."""
+
+from pydantic import BaseModel, Field
+
+
+class PaginationParams(BaseModel):
+    """Re-usable model for pagination."""
+
+    page_index: int = Field(0, ge=0)
+    page_size: int = Field(100, ge=1)
