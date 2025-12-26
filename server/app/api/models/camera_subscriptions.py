8c3c87ab62f2af163d6c74c111947cf052119030
abdmoh123 <abdhawisa@gmail.com>
2025-12-26 02:11:33 +0000

    feat(api): add validation for CameraSubscription model
    IDs must be at least 1.

--- server/app/api/models/camera_subscriptions.py
+++ server/app/api/models/camera_subscriptions.py
@@ -1,13 +1,13 @@
 """File containing pydantic models for camera subscription data."""
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class CameraSubscription(BaseModel):
     """Pydantic model for a subscription of a user to a camera."""
 
-    user_id: int
-    camera_id: int
+    user_id: int = Field(ge=1)
+    camera_id: int = Field(ge=1)
 
     class Config:
         """Config subclass of CameraSubscription."""
