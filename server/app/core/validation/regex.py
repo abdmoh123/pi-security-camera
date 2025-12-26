966b230062733cfc75546642c70ca0c4f11ef9f7
abdmoh123 <abdhawisa@gmail.com>
2025-12-26 22:32:20 +0000

    chore: add comments for the regex validation patterns
    Explains what patterns they match against (e.g. password requirements).

--- server/app/core/validation/regex.py
+++ server/app/core/validation/regex.py
@@ -2,12 +2,21 @@
 
 from typing import Literal
 
-camera_name_regex: Literal[r"^[a-zA-Z]+.*$"]  # As long as the name starts with a letter (case-insensitive)
-host_address_regex: Literal[r"^(((?!25?[6-9])[12]\d|[1-9])?\d\.?\b){4}$"]  # ipv4 pattern
+# As long as the name starts with a letter (case-insensitive)
+camera_name_regex: Literal[r"^[a-zA-Z]+.*$"]
+# Regex pattern for the ipv4 format
+host_address_regex: Literal[r"^(((?!25?[6-9])[12]\d|[1-9])?\d\.?\b){4}$"]
+# Regex pattern for the MAC address format
 mac_address_regex: Literal[r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"]
 
+# Regex pattern to match the email address format (doesn't check if it's real)
 email_regex: Literal[r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$"]
 
+# Passwords must be have the following:
+# - At least 8 characters
+# - At least 1 upper and 1 lower case letter
+# - At least 1 number
+# - At least 1 special character (one of these: @$!%*?&)
 password_regex: Literal[r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"]
 
 # Any name excluding '/' characters
