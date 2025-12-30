40273d8652e3d7b43f0a7f11d201dff7ce91bddf
abdmoh123 <abdhawisa@gmail.com>
2025-12-30 03:04:29 +0000

    test(server): add test for short salts
    argon2 should return an exception if salt is too short.

--- server/tests/core/security/test_hashing.py
+++ server/tests/core/security/test_hashing.py
@@ -1,6 +1,8 @@
 """Tests for the hashing module."""
 
+import pytest
 from argon2 import PasswordHasher
+from argon2.exceptions import HashingError
 
 from app.core.security.hashing import generate_hashed_password
 
@@ -16,3 +18,15 @@ def test_generate_hashed_password() -> None:
 
     hash_without_salt: str = hashed_password.split(":")[1]
     assert ph.verify(hash=hash_without_salt, password=test_password)
+
+
+def test_short_salt() -> None:
+    """Test if the software breaks if the salt is too short."""
+    ph = PasswordHasher()
+    salt: str = "1234"
+    salt_encoding: str = "utf-8"
+
+    test_password: str = "password"
+
+    with pytest.raises(HashingError):
+        _ = generate_hashed_password(test_password, ph, salt, salt_encoding)
