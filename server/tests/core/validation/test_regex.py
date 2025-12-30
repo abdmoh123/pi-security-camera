9e2cdd7ce3791b4825506cc416e2bbd3180fddda
abdmoh123 <abdhawisa@gmail.com>
2025-12-30 19:26:19 +0000

    test(server): camera name regex pattern
    Create tests for valid camera names.

--- server/tests/core/validation/test_regex.py
+++ server/tests/core/validation/test_regex.py
@@ -5,6 +5,14 @@ import re
 import app.core.validation.regex as regex_patterns
 
 
+def test_camera_name_regex() -> None:
+    """Test the camera name regex."""
+    test_camera_names = ["asdf1234", "abc", "a", "camera-1"]
+    pattern = re.compile(regex_patterns.camera_name_regex)
+
+    assert all([pattern.match(name) for name in test_camera_names])
+
+
 def test_email_regex() -> None:
     """Test the email regex pattern against valid emails."""
     test_emails = ["test@example.com", "test@gmail.com", "test@hotmail.com", "test@yahoo.co.uk", "test@protonmail.ch"]
