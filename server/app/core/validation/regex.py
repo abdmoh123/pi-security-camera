cc0661011b61ca6329876faadbc70c8f73c56468
abdmoh123 <abdhawisa@gmail.com>
2026-01-16 00:39:57 +0000

    fix(regex): remove lookaheads in host address regex
    Pydantic doesn't support lookaheads.

--- server/app/core/validation/regex.py
+++ server/app/core/validation/regex.py
@@ -3,7 +3,7 @@
 # As long as the name starts with a letter (case-insensitive)
 camera_name_regex: str = r"^[a-zA-Z]+.*$"
 # Regex pattern for the ipv4 format
-host_address_regex: str = r"^(((?!25?[6-9])[12]\d|[1-9])?\d\.?\b){4}$"
+host_address_regex: str = r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"
 # Regex pattern for the MAC address format
 mac_address_regex: str = r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
 
