22a931b2f04a345a0c283aaa1b7598f34382bf42
abdmoh123 <abdhawisa@gmail.com>
2026-01-20 00:08:43 +0000

    test(server): verify_password functions in core.security
    Test with both valid and invalid password hash pairs.

--- server/tests/core/security/test_hashing.py
+++ server/tests/core/security/test_hashing.py
@@ -4,7 +4,7 @@ import pytest
 from argon2 import PasswordHasher
 from argon2.exceptions import HashingError
 
-from app.core.security.hashing import generate_hashed_password
+from app.core.security.hashing import generate_hashed_password, verify_password
 
 
 def test_generate_hashed_password() -> None:
@@ -20,7 +20,7 @@ def test_generate_hashed_password() -> None:
     assert ph.verify(hash=hash_without_salt, password=test_password)
 
 
-def test_short_salt() -> None:
+def test_generate_hashed_password_short_salt() -> None:
     """Test if the software breaks if the salt is too short."""
     ph = PasswordHasher()
     salt: str = "1234"
@@ -30,3 +30,28 @@ def test_short_salt() -> None:
 
     with pytest.raises(HashingError):
         _ = generate_hashed_password(test_password, ph, salt, salt_encoding)
+
+
+def test_verify_password_valid() -> None:
+    """Test the verify_password function with valid password pairs."""
+    ph = PasswordHasher()
+    salt: str = "12345678"
+    salt_encoding: str = "utf-8"
+
+    test_password: str = "password"
+    test_hashed_password: str = generate_hashed_password(test_password, ph, salt, salt_encoding)
+
+    assert verify_password(test_password, test_hashed_password, ph)
+
+
+def test_verify_password_invalid() -> None:
+    """Test the verify_password function with differing password pairs."""
+    ph = PasswordHasher()
+    salt: str = "12345678"
+    salt_encoding: str = "utf-8"
+
+    test_password: str = "password"
+    test_hashed_password: str = generate_hashed_password(test_password, ph, salt, salt_encoding)
+
+    wrong_passwords: list[str] = ["1234", "PASSWORD", "abcABC123"]
+    assert all([not verify_password(wrong_password, test_hashed_password, ph) for wrong_password in wrong_passwords])
