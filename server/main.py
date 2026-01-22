f3ef72b5adfe560cddf0b5d9cf446e8340f59f22
abdmoh123 <abdhawisa@gmail.com>
2026-01-22 02:56:46 +0000

    refactor(server): move fastapi stuff outside main.py
    main.py now only contains code for running the fastapi app.
    app/app.py contains the code that sets up the application.

--- server/main.py
+++ server/main.py
@@ -1,39 +1,9 @@
 """Pi security project main entrypoint."""
 
-from importlib.metadata import PackageNotFoundError, version
-
 import uvicorn
-from fastapi import FastAPI
 
-from app.api.routes import cameras, users
-from app.auth import routes as auth
-
-# get version info
-try:
-    # get version from installed package metadata
-    __version__ = version("pi-security-camera")
-except PackageNotFoundError:
-    # package is not installed (e.g. in development without editable install)
-    __version__ = "0.1.0"  # fallback initial version value
-
-app = FastAPI(title="Pi Security Camera", description="API for managing Pi security cameras", version=__version__)
-
-api_prefix: str = "/api/v0"
-app.include_router(users.router, prefix=api_prefix)
-app.include_router(cameras.router, prefix=api_prefix)
-app.include_router(auth.router, prefix=api_prefix)
-
-
-@app.get(api_prefix)
-def read_root() -> dict[str, str]:
-    """Root API function."""
-    return {"message": app.title, "description": app.description, "version": app.version}
-
-
-@app.get(f"{api_prefix}/health")
-def check_health() -> dict[str, str]:
-    """Route to check health. Doesn't really do anything yet."""
-    return {"status": "ok"}
+# Allows the main method to run the fast api application
+from app.app import app  # noqa: F401  # pyright: ignore[reportUnusedImport]
 
 
 def main(reload: bool = True) -> None:
