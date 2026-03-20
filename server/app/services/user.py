d54c126a826b7e0d70405b6e52cf8c79202b29c9
abdmoh123 <abdhawisa@gmail.com>
2026-03-20 01:42:35 +0000

    feat(server): allow filtering of users by camera ID and email
    Using the newer sqlalchemy select method.

--- server/app/services/user.py
+++ server/app/services/user.py
@@ -1,6 +1,7 @@
 """File containing crud functions related to the User table."""
 
 from argon2 import PasswordHasher
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.api.models.users import UserCreate, UserUpdate
@@ -28,17 +29,28 @@ def get_user_by_email(db: Session, email: str) -> User | None:
     return db.query(User).filter(User.email == email).first()
 
 
-def get_users(db: Session, user_ids: list[int] | None = None, skip: int = 0, limit: int = 100) -> list[User]:
+def get_users(
+    db: Session,
+    user_ids: list[int] | None = None,
+    email: str | None = None,
+    camera_ids: list[int] | None = None,
+    skip: int = 0,
+    limit: int = 100,
+) -> list[User]:
     """Queries and returns a list of all users with pagination.
 
     If a list of IDs/emails were given, it will only return the given users (if they were found).
     Otherwise, it returns all users in the database (with pagination of course).
     """
-    # Just show all users if the id/emails list is None or empty
-    if not user_ids:
-        return db.query(User).offset(skip).limit(limit).all()
+    query = select(User)
+    if user_ids:
+        query = query.where(User.id.in_(user_ids))
+    if email:
+        query = query.where(User.email.ilike(f"%{email}%"))
+    if camera_ids:
+        query = query.where(User.id.in_(camera_ids))
 
-    return db.query(User).filter(User.id.in_(user_ids)).offset(skip).limit(limit).all()
+    return list(db.execute(query.offset(skip).limit(limit)).scalars().all())
 
 
 def create_user(db: Session, user: UserCreate) -> User:
