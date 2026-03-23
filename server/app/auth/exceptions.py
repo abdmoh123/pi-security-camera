7e458f71ff5efd101b8753d2c17c78f95ad3ed5a
abdmoh123 <abdhawisa@gmail.com>
2026-03-23 03:08:40 +0000

    refactor(server): remove unused auth exceptions
    They may be added back in once I figure out error handling for
    dependencies.

--- server/app/auth/exceptions.py
+++ server/app/auth/exceptions.py
@@ -11,15 +11,3 @@ class TokenDecodingError(Exception):
     """Exception raised when there is an error decoding the access token."""
 
     pass
-
-
-class TokenExpiredError(Exception):
-    """Exception raised when the access token has expired."""
-
-    pass
-
-
-class PermissionDeniedError(Exception):
-    """Exception raised when the user does not have the required permissions."""
-
-    pass
