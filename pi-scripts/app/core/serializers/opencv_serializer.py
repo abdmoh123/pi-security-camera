3f80a660f139ac3df7a29baf954dc1b026780cf8
abdmoh123 <abdhawisa@gmail.com>
2026-04-16 01:27:03 +0100

    chore(pi): suppress mypy and pyright errors for VideoWriter_fourcc
    It actually works, the type checkers just can't find it.
    OpenCV is a C/C++ library after all.

--- pi-scripts/app/core/serializers/opencv_serializer.py
+++ pi-scripts/app/core/serializers/opencv_serializer.py
@@ -3,7 +3,10 @@
 from pathlib import Path
 
 import cv2
-from cv2 import VideoWriter, VideoWriter_fourcc
+from cv2 import (  # type: ignore[attr-defined]
+    VideoWriter,
+    VideoWriter_fourcc,  # pyright: ignore[reportAttributeAccessIssue, reportUnknownVariableType]
+)
 from cv2.typing import MatLike
 
 from app.core.exceptions.serialization import SerializationError
@@ -16,9 +19,9 @@ class OpenCVSerializer:
         """OpenCV based implementation of the write_video method."""
         out = VideoWriter(
             filename=file_path,
-            fourcc=VideoWriter_fourcc(*"mp4v"),
+            fourcc=VideoWriter_fourcc(*"mp4v"),  # pyright: ignore[reportUnknownArgumentType]
             fps=24.0,
-            frameSize=data[0].shape[1::-1],
+            frameSize=data[0].shape[1::-1],  # pyright: ignore[reportAny]
         )
         for frame in data:
             out.write(frame)
