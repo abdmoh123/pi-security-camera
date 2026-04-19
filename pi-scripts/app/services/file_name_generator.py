c70dbd9b97e9cec7c2d26889481d013a8e9dfb04
abdmoh123 <abdhawisa@gmail.com>
2026-04-19 17:54:04 +0100

    docs(pi): complete docstrings for file_name_generator functions

--- pi-scripts/app/services/file_name_generator.py
+++ pi-scripts/app/services/file_name_generator.py
@@ -6,12 +6,26 @@ __timestamp_format: str = "%Y-%m-%d_%H-%M-%S"
 
 
 def generate_timestamp_video_name(file_ext: str = "mp4") -> str:
-    """Generates a timestamped video name."""
+    """Generates a timestamped video name.
+
+    Args:
+        file_ext: The file extension to use. Defaults to "mp4".
+
+    Returns:
+        The timestamped video name.
+    """
     current_ts: str = datetime.datetime.now().strftime(__timestamp_format)
     return f"video-{current_ts}.{file_ext}"
 
 
 def generate_timestamp_photo_name(file_ext: str = "jpg") -> str:
-    """Generates a timestamped video name."""
+    """Generates a timestamped video name.
+
+    Args:
+        file_ext: The file extension to use. Defaults to "jpg".
+
+    Returns:
+        The timestamped photo name.
+    """
     current_ts: str = datetime.datetime.now().strftime(__timestamp_format)
     return f"photo-{current_ts}.{file_ext}"
