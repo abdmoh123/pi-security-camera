254efc1a63be9b4e2e76618311011f1777c984de
abdmoh123 <abdhawisa@gmail.com>
2026-05-05 00:58:51 +0100

    feat(camera): convert API model dataclasses to pydantic models
    Sadly resulted in code duplication from the server module (different
    sub-project). Perhaps creating a core lib sub-module might fix this
    (share the core lib between the server and client). Although this won't
    solve any issues if the sub-projects are written in different languages
    (e.g. flutter client and fastapi backend).

--- camera/app/core/models/user.py
+++ camera/app/core/models/user.py
@@ -1,10 +1,12 @@
 """Model for a user's data."""
 
-from dataclasses import dataclass
+from pydantic import BaseModel, EmailStr, Field
+
+# Regex pattern to match the email address format (doesn't check if it's real)
+email_regex: str = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$"
 
 
-@dataclass(frozen=True)
-class User:
+class User(BaseModel):
     """Model for a user's data gained from the server API.
 
     Attributes:
@@ -13,6 +15,11 @@ class User:
         is_admin: A flag for whether the user is an admin
     """
 
-    id: str
-    email: str
-    is_admin: bool
+    id: int = Field(ge=1)
+    email: EmailStr = Field(pattern=email_regex)
+    is_admin: bool = Field(default=False)
+
+    class Config:
+        """Config subclass of UserResponse."""
+
+        from_attributes: bool = True
--- camera/app/core/models/video.py
+++ camera/app/core/models/video.py
@@ -1,10 +1,12 @@
 """Model for video data from the server."""
 
-from dataclasses import dataclass
+from pydantic import BaseModel, Field
+
+# Any name excluding '/' characters
+file_name_regex: str = r"video-\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.mp4"
 
 
-@dataclass(frozen=True)
-class Video:
+class Video(BaseModel):
     """Model for a video entry data gained from the server API.
 
     Attributes:
@@ -12,5 +14,11 @@ class Video:
         file_name: A video's file name
     """
 
-    id: str
-    file_name: str
+    id: int = Field(ge=1)
+    file_name: str = Field(pattern=file_name_regex, min_length=5)
+    camera_id: int = Field(ge=1)
+
+    class Config:
+        """Config subclass of Video."""
+
+        from_attributes: bool = True
