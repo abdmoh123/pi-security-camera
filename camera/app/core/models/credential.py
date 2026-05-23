c85e72b84bd6b6214e713b3e75371b223e45abb8
abdmoh123 <abdhawisa@gmail.com>
2026-05-24 00:23:39 +0100

    feat(camera): create Credential pydantic model
    To be used for communicating with the server's API.

---
+++ camera/app/core/models/credential.py
@@ -0,0 +1,49 @@
+"""Model for a camera credential."""
+
+from pydantic import BaseModel, Field, field_validator
+
+
+class Credential(BaseModel):
+    """Model for a camera's credential.
+
+    Used to authenticate with the server's API.
+
+    Attributes:
+        client_id: The client ID
+        user_id: The user/owner's ID
+        client_secret: The client secret
+    """
+
+    client_id: str
+    user_id: int = Field(ge=1)
+    client_secret: str
+
+    @field_validator('client_id', mode='after')
+    @classmethod
+    def _verify_client_id(cls, value: str) -> str:
+        """Client ID should follow format <user_name>:<bunch-of-chars>.
+
+        Args:
+            value: The client ID
+
+        Returns:
+            The client ID if the format is correct
+
+        Raises:
+            ValueError: If the client ID is not in the correct format
+        """
+        if (len(value.split(':')) != 2):
+            raise ValueError("Client ID is not in the correct format")
+        return value
+
+    def get_user_name_quick(self) -> str:
+        """Returns a name linked to this credential.
+
+        The client ID contains a user name in it, so this is a quick way to get
+        some form of name. This is not guaranteed to be unique, so it is
+        preferred to call the server's API instead for a more accurate username.
+
+        Returns:
+            The user name
+        """
+        return self.client_id.split(':')[0]
