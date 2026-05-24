01fc76e4910125610e842fb48d821d37ba25b488
abdmoh123 <abdhawisa@gmail.com>
2026-05-24 01:11:57 +0100

    feat(camera): create data directory reader
    For non-video related data to be serialised and re-used. For example the
    credentials used to connect and authenticate with the server API.

---
+++ camera/app/services/data_reader.py
@@ -0,0 +1,65 @@
+"""Reads data from a given data folder."""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+from app.core.models.credential import Credential
+
+
+@dataclass(frozen=True)
+class DataReader:
+    """Reads data from a given data folder.
+
+    Attributes:
+        data_directory: The directory to read data from.
+    """
+
+    data_directory: Path
+
+    def __post_init__(self) -> None:
+        """Perform some validation on the DataReader attributes."""
+        if not self.data_directory.exists():
+            raise FileNotFoundError(
+                f"Data directory not found at {self.data_directory}"
+            )
+
+    def credentials_file_exists(self) -> bool:
+        """Check if the credentials file exists.
+
+        Returns:
+            True if the credentials file exists, False otherwise.
+        """
+        return (self.data_directory / "credentials.json").exists()
+
+    def read_credentials(self) -> Credential:
+        """Read the camera credentials from the data directory.
+
+        Returns:
+            The camera credentials.
+        """
+        credentials_path = self.data_directory / "credentials.json"
+        if not self.credentials_file_exists():
+            raise FileNotFoundError(
+                f"Credentials file not found at {credentials_path}"
+            )
+
+        return Credential.model_validate_json(credentials_path.read_text())
+
+
+def prepare_data_reader(data_directory: Path) -> None:
+    """Does some preparation for the DataReader."""
+    data_directory.resolve().mkdir(parents=True, exist_ok=True)
+
+
+def setup_data_reader(data_directory: Path) -> DataReader:
+    """Creates a DataReader after doing some preparation and input validation.
+
+    Args:
+        data_directory: The directory to read data from.
+
+    Returns:
+        The DataReader.
+    """
+    resolved_data_directory = data_directory.resolve()
+    prepare_data_reader(resolved_data_directory)
+    return DataReader(resolved_data_directory)
