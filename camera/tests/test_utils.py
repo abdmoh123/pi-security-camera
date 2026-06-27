aa4a71325267f1d6b7a64a3ce3fdee8da1c3c9de
abdmoh123 <abdhawisa@gmail.com>
2026-06-28 00:15:49 +0100

    test(camera): fix test_get_mac_address for windows machines

--- camera/tests/test_utils.py
+++ camera/tests/test_utils.py
@@ -1,5 +1,6 @@
 """Tests for utils package."""
 
+import os
 import re
 import subprocess
 
@@ -14,12 +15,35 @@ def test_get_mac_address() -> None:
     assert len(mac_address) == 17
     assert re.match(mac_address_regex, mac_address)
 
-    res = subprocess.run(
-        ["ip", "link", "show", "eth0"],
-        capture_output=True,
-        text=True,
-    )
-    if res.returncode != 0:
-        raise Exception(res.stderr)
+    actual_mac_address_content = ""
+    match os.name:
+        case "posix":
+            res = subprocess.run(
+                ["ip", "link", "show"],
+                capture_output=True,
+                text=True,
+            )
+            if res.returncode != 0:
+                raise Exception(res.stderr)
 
-    assert mac_address in res.stdout
+            actual_mac_address_content = res.stdout
+        case "nt":
+            res = subprocess.run(
+                ["ipconfig", "/all"],
+                capture_output=True,
+                text=True,
+            )
+            if res.returncode != 0:
+                raise Exception(res.stderr)
+
+            split_stdout = res.stdout.split("\n")
+            mac_address_lines = [
+                line.replace("-", ":")
+                for line in split_stdout
+                if "Physical Address" in line
+            ]
+            actual_mac_address_content = "\n".join(mac_address_lines)
+        case _:
+            raise Exception("Unsupported platform")
+
+    assert mac_address in actual_mac_address_content
