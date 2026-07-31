6e6de61ec84d7f45791f22096591ce2a0c14374a
abdmoh123 <abdhawisa@gmail.com>
2026-07-31 01:24:01 +0100

    feat(client): add helper functions for TokenType
    Converting to a label (useful for authorization headers), and creating
    the enum from a string.

--- client/lib/types/token_type.dart
+++ client/lib/types/token_type.dart
@@ -1,1 +1,13 @@
-enum TokenType { bearer }
+enum TokenType {
+  bearer;
+
+  static TokenType fromName(String name) =>
+      TokenType.values.firstWhere((e) => e.name == name);
+
+  String get label {
+    switch (this) {
+      case TokenType.bearer:
+        return 'Bearer';
+    }
+  }
+}
