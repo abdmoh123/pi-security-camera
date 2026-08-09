134a750cb0cb929e6c8bf1b0f44ee16e276ee044
abdmoh123 <abdhawisa@gmail.com>
2026-08-09 14:50:01 +0200

    fix(client): layout of the video card widget
    Put the datetime stuff and 3 dots to the right, and the video and camera
    names to the left.

--- client/lib/widgets/video_card.dart
+++ client/lib/widgets/video_card.dart
@@ -18,33 +18,55 @@ class VideoCard extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(8.0),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text(video.fileName),
-                Text("camera-${video.cameraID.toString()}"),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(video.fileName, textAlign: TextAlign.left),
+                    Text(
+                      "camera-${video.cameraID.toString()}",
+                      textAlign: TextAlign.left,
+                    ),
+                  ],
+                ),
               ],
             ),
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text(DateFormat.Hm(locale).format(video.uploadedAt)),
-                Text(DateFormat.yMd(locale).format(video.uploadedAt)),
-              ],
-            ),
-            PopupMenuButton<MenuAction>(
-              onSelected: (value) {
-                switch (value) {
-                  case MenuAction.delete:
-                    break;
-                }
-              },
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  value: MenuAction.delete,
-                  child: ListTile(
-                    leading: Icon(Icons.delete),
-                    title: Text("Delete"),
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      DateFormat.Hm(locale).format(video.uploadedAt),
+                      textAlign: TextAlign.right,
+                    ),
+                    Text(
+                      DateFormat.yMd(locale).format(video.uploadedAt),
+                      textAlign: TextAlign.right,
+                    ),
+                  ],
+                ),
+                PopupMenuButton<MenuAction>(
+                  onSelected: (value) {
+                    switch (value) {
+                      case MenuAction.delete:
+                        break;
+                    }
+                  },
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      value: MenuAction.delete,
+                      child: ListTile(
+                        leading: Icon(Icons.delete),
+                        title: Text("Delete"),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
