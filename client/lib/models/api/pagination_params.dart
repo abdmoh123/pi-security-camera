d8ebe07a8d8a97675fa09fed5e6225f06d48654d
abdmoh123 <abdhawisa@gmail.com>
2026-08-11 19:56:11 +0200

    feat(client): make PaginationParams json serialisable

--- client/lib/models/api/pagination_params.dart
+++ client/lib/models/api/pagination_params.dart
@@ -1,6 +1,14 @@
-class PaginationParams {
+import 'package:pisec_client/models/json_serialisable.dart';
+
+class PaginationParams implements JsonSerialisable {
   final int pageIndex;
   final int pageSize;
 
   const PaginationParams({this.pageIndex = 0, this.pageSize = 100});
+
+  @override
+  Map<String, dynamic> toJson() => {
+    'page_index': pageIndex,
+    'page_size': pageSize,
+  };
 }
