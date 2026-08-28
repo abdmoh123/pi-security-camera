6e8ad0903b6b1f02cc385c60aeec70e732973a53
abdmoh123 <abdhawisa@gmail.com>
2026-08-28 23:51:17 +0100

    fix(client): remove trailing &s if query object is empty

--- client/lib/models/http/http_queryable.dart
+++ client/lib/models/http/http_queryable.dart
@@ -2,6 +2,9 @@ abstract interface class PathQueryable {
   String toPathQueryString();
 
   static String combineToPathQueryString(Iterable<PathQueryable> queries) {
-    return queries.map((q) => q.toPathQueryString()).join('&');
+    return queries
+        .where((q) => q.toPathQueryString().isNotEmpty)
+        .map((q) => q.toPathQueryString())
+        .join('&');
   }
 }
