63c2f0db28e405cb963f7f39f9dcd1c23edadbe9
abdmoh123 <abdhawisa@gmail.com>
2026-08-30 22:21:50 +0100

    chore(client): use indexOf and throw error if video index wasn't found

--- client/lib/services/task_id_generators.dart
+++ client/lib/services/task_id_generators.dart
@@ -1,19 +1,17 @@
 import 'package:pisec_client/exceptions/download_exceptions.dart';
 
 String videoIdFromUrl(String url) {
-  final splitUrl = url.split("/");
-  int videoIndex = 0;
-  for (var i = 0; i < splitUrl.length; i++) {
-    if (splitUrl[i] == "videos") {
-      break;
-    }
-    videoIndex++;
+  final List<String> splitUrl = url.split("/");
+
+  final int videoIndex = splitUrl.indexOf("videos");
+  if (videoIndex == -1) {
+    throw IdGenerationException("Failed to parse video id from url");
   }
 
   final videoId = int.tryParse(splitUrl[videoIndex + 1]);
-
   if (videoId == null) {
     throw IdGenerationException("Failed to parse video id from url");
   }
+
   return "pisec.videos.$videoId";
 }
