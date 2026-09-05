b3ede92fe41760c695c8a729fb65302f166e882a
abdmoh123 <abdhawisa@gmail.com>
2026-09-05 21:59:56 +0100

    feat(client): settings page layout
    No functionality yet

--- client/lib/screens/settings.dart
+++ client/lib/screens/settings.dart
@@ -5,12 +5,53 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    return ListView(
       children: [
-        Container(
-          padding: EdgeInsetsGeometry.all(8.0),
-          child: Text("Settings", textAlign: TextAlign.center),
+        ListTile(
+          leading: Icon(Icons.account_circle),
+          title: Text("Profile"),
+          subtitle: Text("Change account details"),
+        ),
+        ListTile(
+          leading: Icon(Icons.key),
+          title: Text("Credentials"),
+          subtitle: Text("Manage camera credentials"),
+        ),
+        ListTile(
+          leading: Icon(Icons.notifications),
+          title: Text("Notifications"),
+          subtitle: Text("Adjust notification preferences"),
+        ),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text("App settings"),
+          subtitle: Text("Adjust preferences"),
+        ),
+        ListTile(
+          leading: Icon(Icons.help),
+          title: Text("Help"),
+          subtitle: Text("View tutorials and documentation"),
+        ),
+        ListTile(
+          leading: Icon(Icons.info),
+          title: Text("About"),
+          subtitle: Text("View app and server info"),
+        ),
+        Center(
+          child: FittedBox(
+            child: TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll<Color>(
+                  Theme.of(context).colorScheme.errorContainer,
+                ),
+                foregroundColor: WidgetStatePropertyAll<Color>(
+                  Theme.of(context).colorScheme.onErrorContainer,
+                ),
+              ),
+              child: Row(children: [Icon(Icons.logout), Text("Sign out")]),
+            ),
+          ),
         ),
       ],
     );
